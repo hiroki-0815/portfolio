0a2cf46dcf0b9a3428af4d6d4850b0db4e8ad55e
hiroki <haohuirixia851@gmail.com>
2025-02-14 09:38:03 +0900

    build portfolio

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,14 @@
 import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import ProjectsSection from "@/sections/Projects";
-import Tape from "@/sections/Tape";
-import About from "@/sections/About";
 import Footer from "@/sections/Footer";
 
 export default function Home() {
   return (
-    <div className="px-3">
+    <div className="px-3 md:px-7 py-5">
       <Header />
       <HeroSection />
       <ProjectsSection />
-      <Tape />
-      <About />
       <Footer />
     </div>
   );
