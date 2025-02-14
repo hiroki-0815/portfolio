f10da842d0b3d10302cd6f522b1db0795361eeb7
hiroki <haohuirixia851@gmail.com>
2025-02-14 09:39:23 +0900

    build portfolio

--- frontend/src/sections/Hero.tsx
+++ frontend/src/sections/Hero.tsx
@@ -1,152 +1,30 @@
 "use client";
 
-import memojiImage from "@/assets/images/memoji-computer.png";
+import me from "@/assets/images/me.png";
 import Image from "next/image";
-import grainImage from "@/assets/images/grain.jpg";
-import HeroOrbit from "@/components/HeroOrbit";
-import StarIcon from "@/assets/icons/star.svg";
-import SparkleIcon from "@/assets/icons/sparkle.svg";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
-      <div className="absolute inset-0">
-        <div
-          className="absolute inset-0 -z-30 opacity-5"
-          style={{
-            backgroundImage: `url(${grainImage.src})`,
-          }}
-        ></div>
-        <div className="size-[620px] hero-ring"></div>
-        <div className="size-[820px] hero-ring"></div>
-        <div className="size-[1020px] hero-ring"></div>
-        <div className="size-[1220px] hero-ring"></div>
-
-        <HeroOrbit
-          size={430}
-          rotation={-15}
-          shouldOrbit
-          orbitDuration="30s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-8 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={440}
-          rotation={79}
-          shouldOrbit
-          orbitDuration="32s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-5 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={530}
-          rotation={178}
-          shouldOrbit
-          orbitDuration="34s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-10 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={520}
-          rotation={-42}
-          shouldOrbit
-          orbitDuration="36s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={550}
-          rotation={20}
-          shouldOrbit
-          orbitDuration="38s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <StarIcon className="size-12 text-emerald-600" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={590}
-          rotation={98}
-          shouldOrbit
-          orbitDuration="40s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <StarIcon className="size-8 text-emerald-600" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={650}
-          rotation={-5}
-          shouldOrbit
-          orbitDuration="42s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-2 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={710}
-          rotation={144}
-          shouldOrbit
-          orbitDuration="44s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-14 text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={720}
-          rotation={85}
-          shouldOrbit
-          orbitDuration="46s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <SparkleIcon className="size-3 rounded-full text-emerald-300/20" />
-        </HeroOrbit>
-        <HeroOrbit
-          size={800}
-          rotation={-72}
-          shouldOrbit
-          orbitDuration="48s"
-          shouldSpin
-          spinDuration="10s"
-        >
-          <StarIcon className="size-28 text-emerald-600" />
-        </HeroOrbit>
-      </div>
+    <div className="py-10 relative z-0 overflow-x-clip">
       <div className="container">
         <div className=" flex flex-col items-center">
           <Image
-            src={memojiImage}
-            className="size-[100px]"
+            src={me}
+            className="w-[900px] rounded-md"
             alt="Person peeking from behind laptop"
           />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-2 rounded-lg">
-            <div className="bg-green-400 size-2.5 rounded-full relative">
-              <div className="bg-green-400 size-2.5 rounded-full animate-ping-large absolute inset-0"></div>
-            </div>
-            <div className="text-sm font-semibold">
-              Available for new projects
-            </div>
-          </div>
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-2 rounded-lg"></div>
         </div>
-        <div className="max-w-lg mx-auto">
+        <div className="mx-auto">
           <h1 className="font-serif text-3xl md:text-5xl text-center mt-8">
-            Buliding Exceptional User Experiences
+            Hiroki Kusaka
           </h1>
-          <p className="mt-4 text-center text-white/70 md:text-lg">
-            I specialize in transforming designs into functional,
-            high-performing web applications. Let&apos;s discuss your next
-            project.
+          <p className="mt-4 px-5 py-4 z-50 text-center rounded-md text-white bg-slate-700 md:text-lg">
+            Hi, I'm Hiroki! I'm currently studying to become a software
+            engineer, with a strong focus on JavaScript and its ecosystem. I
+            specialize in TypeScript for type safety and maintainability, React
+            for building dynamic user interfaces, and Express.js for backend
+            development.
           </p>
         </div>
       </div>
