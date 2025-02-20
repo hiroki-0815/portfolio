e60e845e6fc993b64b1c0e9f400efbafb0c6e93c
hiroki <haohuirixia851@gmail.com>
2025-02-20 11:55:01 +0900

    fix bug

--- src/app/section/Footer.tsx
+++ src/app/section/Footer.tsx
@@ -5,15 +5,7 @@ const Footer = () => {
     <footer className="relative overflow-x-clip">
       <div className="absolute h-[50px] w-[1600px] bottom-0 left-1/2 -translate-x-1/2 -z-10"></div>
       <div className="container mx-auto">
-        <div className="border-t border-white/15 py-6 text-sm flex flex-col items-center text-center">
-          <a
-            href="https://github.com/hiroki-0815/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mt-4 px-8 py-2 bg-white text-gray-900 text-[14px] rounded hover:bg-gray-200 transition mb-4 inline-block"
-          >
-            Visit GitHub
-          </a>
+        <div className="border-white/15 py-6 text-sm flex flex-col items-center text-center">
           <div className="text-white/70">&copy; 2024. All rights reserved.</div>
         </div>
       </div>
--- src/app/section/Hero.tsx
+++ src/app/section/Hero.tsx
@@ -15,6 +15,14 @@ export const HeroSection = () => {
         <h1 className="font-serif text-white text-3xl md:text-5xl text-center mt-8">
           {t("name")}
         </h1>
+        <a
+          href="https://github.com/hiroki-0815/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-4 px-8 py-2 bg-white text-gray-900 text-[14px] rounded hover:bg-gray-200 transition mb-4 inline-block"
+        >
+          Visit GitHub
+        </a>
         <p className="mt-4 px-5 py-4 z-50 text-center rounded-md text-white bg-slate-700 md:text-lg">
           {t("description")}
         </p>
