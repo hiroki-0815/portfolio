6086bf1c229e4e3d28394f0d1560c13b9bcfb8a0
hiroki <haohuirixia851@gmail.com>
2025-02-22 17:24:07 +0900

    fix bug

--- src/app/components/LocalSwitcher.tsx
+++ src/app/components/LocalSwitcher.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { useLocale } from "next-intl";
 import { useRouter } from "next/navigation";
 import React, { ChangeEvent } from "react";
 
 const LocalSwitcher = () => {
   const router = useRouter();
+  const locale = useLocale();
 
   const onSelectChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const selectedLocale = e.target.value;
@@ -14,6 +16,7 @@ const LocalSwitcher = () => {
   return (
     <select
       onChange={onSelectChange}
+      value={locale}
       className="nav-item text-white/80 hover:text-white bg-transparent border-none outline-none cursor-pointer"
     >
       <option className="text-gray-900" value="ja">
